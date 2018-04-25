93ee59640f8e175858343d35a6ffc2565e6f4795
C. Evan Smith <cesmith50@gmail.com>
2018-04-25 03:15:46 -0500

    script fixin

--- nucleus3.py
+++ nucleus3.py
@@ -41,10 +41,11 @@ import json
 import datetime
 import numpy as np
 import skimage.io
+import pathlib
 from imgaug import augmenters as iaa
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = os.path.abspath("/home/user/ec2-user/histopath")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -53,8 +54,6 @@ from mrcnn import utils
 from mrcnn import model as modellib
 from mrcnn import visualize
 
-# Path to trained weights file
-COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
@@ -62,37 +61,8 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Results directory
 # Save submission files here
-RESULTS_DIR = os.path.join(ROOT_DIR, "results/nucleus/")
+RESULTS_DIR = os.path.join(ROOT_DIR, "results")
 
-# The dataset doesn't have a standard train/val split, so I picked
-# a variety of images to surve as a validation set.
-VAL_IMAGE_IDS = [
-    "0c2550a23b8a0f29a7575de8c61690d3c31bc897dd5ba66caec201d201a278c2",
-    "92f31f591929a30e4309ab75185c96ff4314ce0a7ead2ed2c2171897ad1da0c7",
-    "1e488c42eb1a54a3e8412b1f12cde530f950f238d71078f2ede6a85a02168e1f",
-    "c901794d1a421d52e5734500c0a2a8ca84651fb93b19cec2f411855e70cae339",
-    "8e507d58f4c27cd2a82bee79fe27b069befd62a46fdaed20970a95a2ba819c7b",
-    "60cb718759bff13f81c4055a7679e81326f78b6a193a2d856546097c949b20ff",
-    "da5f98f2b8a64eee735a398de48ed42cd31bf17a6063db46a9e0783ac13cd844",
-    "9ebcfaf2322932d464f15b5662cae4d669b2d785b8299556d73fffcae8365d32",
-    "1b44d22643830cd4f23c9deadb0bd499fb392fb2cd9526d81547d93077d983df",
-    "97126a9791f0c1176e4563ad679a301dac27c59011f579e808bbd6e9f4cd1034",
-    "e81c758e1ca177b0942ecad62cf8d321ffc315376135bcbed3df932a6e5b40c0",
-    "f29fd9c52e04403cd2c7d43b6fe2479292e53b2f61969d25256d2d2aca7c6a81",
-    "0ea221716cf13710214dcd331a61cea48308c3940df1d28cfc7fd817c83714e1",
-    "3ab9cab6212fabd723a2c5a1949c2ded19980398b56e6080978e796f45cbbc90",
-    "ebc18868864ad075548cc1784f4f9a237bb98335f9645ee727dac8332a3e3716",
-    "bb61fc17daf8bdd4e16fdcf50137a8d7762bec486ede9249d92e511fcb693676",
-    "e1bcb583985325d0ef5f3ef52957d0371c96d4af767b13e48102bca9d5351a9b",
-    "947c0d94c8213ac7aaa41c4efc95d854246550298259cf1bb489654d0e969050",
-    "cbca32daaae36a872a11da4eaff65d1068ff3f154eedc9d3fc0c214a4e5d32bd",
-    "f4c4db3df4ff0de90f44b027fc2e28c16bf7e5c75ea75b0a9762bbb7ac86e7a3",
-    "4193474b2f1c72f735b13633b219d9cabdd43c21d9c2bb4dfc4809f104ba4c06",
-    "f73e37957c74f554be132986f38b6f1d75339f636dfe2b681a0cf3f88d2733af",
-    "a4c44fc5f5bf213e2be6091ccaed49d8bf039d78f6fbd9c4d7b7428cfcb2eda4",
-    "cab4875269f44a701c5e58190a1d2f6fcb577ea79d842522dcab20ccb39b7ad2",
-    "8ecdb93582b2d5270457b36651b62776256ade3aaa2d7432ae65c14f07432d49",
-]
 
 
 ############################################################
@@ -182,7 +152,21 @@ class NucleusInferenceConfig(NucleusConfig):
 
 class NucleusDataset(utils.Dataset):
 
-    def load_nucleus(self, dataset_dir, subset):
+    def fix_id(self,training_path):
+        """
+        takes path, strips it to ID
+        """
+        i = str(training_path)[-9:-8]
+        if str(training_path)[29:30].lower() == "m":
+            M = 1
+    
+        else:
+            M = 0
+
+        image_id = str(training_path)[22:28] + "_{}_{}".format(M,i)
+        return image_id
+
+    def load_nucleus(self, dataset_dir, name="nucleus", subset):
         """Load a subset of the nuclei dataset.
 
         dataset_dir: Root directory of the dataset
@@ -193,29 +177,28 @@ class NucleusDataset(utils.Dataset):
         """
         # Add classes. We have one class.
         # Naming the dataset nucleus, and the class nucleus
-        self.add_class("nucleus", 1, "nucleus")
+
+        self.add_class(name, 1, subset)
 
         # Which subset?
-        # "val": use hard-coded list above
+        # "val": use hard-coded list below:
+        
         # "train": use data from stage1_train minus the hard-coded list above
         # else: use the data from the specified sub-directory
-        assert subset in ["train", "val", "stage1_train", "stage1_test", "stage2_test"]
-        subset_dir = "stage1_train" if subset in ["train", "val"] else subset
-        dataset_dir = os.path.join(dataset_dir, subset_dir)
-        if subset == "val":
-            image_ids = VAL_IMAGE_IDS
-        else:
-            # Get image ids from directory names
-            image_ids = next(os.walk(dataset_dir))[1]
-            if subset == "train":
-                image_ids = list(set(image_ids) - set(VAL_IMAGE_IDS))
+        assert subset in ["train", "test", "val", "mask"]
+        dataset_dir = os.path.join("./data/", subset)
+     
+        glob = pathlib.Path(dataset_dir).glob('*.TIF')
+        pathlist = sorted([x for x in glob])
+        image_ids = [fix_id(path) for path in pathlist]
+
 
         # Add images
         for image_id in image_ids:
             self.add_image(
-                "nucleus",
+                name,
                 image_id=image_id,
-                path=os.path.join(dataset_dir, image_id, "images/{}.png".format(image_id)))
+                path=pathlist[image_ids.index(image_id)])
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
